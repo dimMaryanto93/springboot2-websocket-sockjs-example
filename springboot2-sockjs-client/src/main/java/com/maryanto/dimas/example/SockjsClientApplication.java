b791189159996f3c17a3b554c19ee36860b2d6fb
dimasm93 <software.dimas_m@icloud.com>
2019-09-03 11:01:07 +0700

    init project springboot websocket

---
+++ springboot2-sockjs-client/src/main/java/com/maryanto/dimas/example/SockjsClientApplication.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockjsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SockjsClientApplication.class, args);
+	}
+
+}
---
+++ springboot2-websocket-server/src/main/java/com/maryanto/dimas/example/config/WebSocketMessageBroker.java
@@ -0,0 +1,12 @@
+package com.maryanto.dimas.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketMessageBroker implements WebSocketMessageBrokerConfigurer {
+
+
+}
